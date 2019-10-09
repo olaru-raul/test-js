d3ae8a3898b0a72eb1637cbb64cc0d0d41734507
Raul Olaru <raul.olaru@accesa.eu>
2019-10-09 13:30:46 +0300

    test

--- js-merge-objects.js
+++ js-merge-objects.js
@@ -1,7 +1,6 @@
-function mergeTwoObjects (obj1, obj2) 
-{
-    for (var p in obj2) {
-  	    obj1[p] = obj2[p];
-	  }
-	  return obj1;
+function mergeTwoObjects(obj1, obj2) {
+	for (var p in obj2) {
+		obj1[p] = obj2[p];
+	}
+	return obj1;
 }
